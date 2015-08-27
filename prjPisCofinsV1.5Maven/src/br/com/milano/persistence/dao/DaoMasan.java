73d2c46586c3a73cf4abdd0a2e69aaf448b863d5
Vitor Felipe <vitorfelipep@users.noreply.github.com>
2015-08-27 12:12:40 -0300

    Update DaoMasan.java

--- prjPisCofinsV1.5Maven/src/br/com/milano/persistence/dao/DaoMasan.java
+++ prjPisCofinsV1.5Maven/src/br/com/milano/persistence/dao/DaoMasan.java
@@ -11,8 +11,8 @@ public class DaoMasan {
 	Connection con;
 
 	private final String URL = "jdbc:oracle:thin:@//192.168.15.182:1521/MASAN";
-	private final String USER = "sapiens";
-	private final String PASS = "sapiens";
+	private final String USER = "******";
+	private final String PASS = "******";
 
 	protected void open() throws Exception {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
