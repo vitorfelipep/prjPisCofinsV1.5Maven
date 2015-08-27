128d04abb58c163a3cc077e51142c5e5639fb655
Vitor Felipe <vitorfelipep@users.noreply.github.com>
2015-08-27 12:12:05 -0300

    Update DaoMilano.java

--- prjPisCofinsV1.5Maven/src/br/com/milano/persistence/dao/DaoMilano.java
+++ prjPisCofinsV1.5Maven/src/br/com/milano/persistence/dao/DaoMilano.java
@@ -11,8 +11,8 @@ public class DaoMilano {
 	Connection con;
 
 	private final String URL = "jdbc:oracle:thin:@//192.168.15.182:1521/MILANO";
-	private final String USER = "sapiens2";
-	private final String PASS = "senior2";
+	private final String USER = "*******";
+	private final String PASS = "*******";
 
 	protected void open() throws Exception {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
